a95fdf9a7a961d9a615fb887ac2ab531821c9888
Luverine <chandanprasaddas02@gmail.com>
2025-01-28 15:43:59 +0530

    new code implimented

--- src/Java/Bigginer/TaskTracker/TaskManager.java
+++ src/Java/Bigginer/TaskTracker/TaskManager.java
@@ -2,18 +2,6 @@ package Java.Bigginer.TaskTracker;
 
 public class TaskManager {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("User");
-        System.out.println("code  will be ");
-        System.out.println("uploaded wait");
-        System.out.println("uploaded wait"); System.out.println("uploaded wait");
-        System.out.println("uploaded wait");System.out.println("uploaded wait");System.out.println("uploaded wait");
-em.out.println("Hello");
-        System.out.println("User");
-        System.out.println("code  will be ");
-        System.out.println("uploaded wait");
-        System.out.println("uploaded wait"); System.out.println("uploaded wait");
-        System.out.println("uploaded wait");System.out.println("uploaded wait");System.out.println("uploaded wait");
-
+        System.out.println("Wellcome");
     }
 }
