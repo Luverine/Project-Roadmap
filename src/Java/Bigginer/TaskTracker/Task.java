971a6888e2a6662c43e668fff02076bc32a4512a
Luverine <chandanprasaddas02@gmail.com>
2025-02-06 21:23:27 +0530

    new code implimented

--- src/Java/Bigginer/TaskTracker/Task.java
+++ src/Java/Bigginer/TaskTracker/Task.java
@@ -1,7 +1,13 @@
 package Java.Bigginer.TaskTracker;
 
 public class Task {
-    // Implement Task Structure
+    private int id;
+    private String description;
+    private String status;
+    private String createdAt;
+    private String updatedAt;
+
+    
 
 
 
