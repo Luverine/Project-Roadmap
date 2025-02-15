079a2f7387757372fed6247741c57eb19fa5e363
Luverine <chandanprasaddas02@gmail.com>
2025-02-15 23:23:25 +0530

    new code implimented

--- src/Java/Bigginer/TaskTracker/Task.java
+++ src/Java/Bigginer/TaskTracker/Task.java
@@ -2,8 +2,8 @@ package Java.Bigginer.TaskTracker;
 
 public class Task {
     private final int id;
-    private final String description;
-    private final String status;
+    private String description;
+    private String status;
     private final String createdAt;
     private final String updatedAt;
 
@@ -20,6 +20,20 @@ public class Task {
     public int getId(){ return id; }
     public String getDescription(){ return description; }
     public String getCreatedAt(){ return createdAt; }
+    public String getUpdatedAt(){ return updatedAt; }
+    public String getStatus(){ return status; }
+
+    // Setters
+    public void setDescription(String description){
+        this.description = description;
+        updateTimestamp();
+    }
+
+    public void setStatus(String status){
+        if (status.equals("todo") || status.equals("in-progress") || status.equals("Completed")){
+            this.status = status;
+            updateTimestamp();
+        } else { throw new IllegalArgumentException("Invalid status" + status); }
 
     // Helper methods
     private String getCurrentTimestamp() { return ""; }
